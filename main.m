d811e5c62916e198a9643d83a53669dbe5edc559
Aether <aether@qq.com>
2014-06-25 14:34:47 -0400

    Created

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Arduino Serial Example
+//
+//  Created by Gabe Ghearing on 6/30/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
